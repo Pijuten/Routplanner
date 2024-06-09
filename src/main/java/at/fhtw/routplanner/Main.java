46ff57fcd8ca35218814ff0b2c929d3b753621b3
berkkai <129563548+berkkai@users.noreply.github.com>
2024-06-09 17:32:16 +0200

    Styling

--- src/main/java/at/fhtw/routplanner/Main.java
+++ src/main/java/at/fhtw/routplanner/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.apache.log4j.Logger;
 
+import java.net.URL;
 import java.util.Locale;
 
 public class Main extends Application {
@@ -24,24 +25,31 @@ public class Main extends Application {
         log.info("Stage started");
     }
 
-    public static Parent showStage(Stage primaryStage) throws Exception{
+    public static Parent showStage(Stage primaryStage) throws Exception {
         Parent root = FXMLDependencyInjection.load("main.fxml", Locale.GERMAN);
         Scene scene = new Scene(root);
 
-        // Set the scene on the stage
+        String cssPath = "/at/fhtw/routplanner/view/styles.css";
+        URL cssURL = Main.class.getResource(cssPath);
+        if (cssURL != null) {
+            scene.getStylesheets().add(cssURL.toExternalForm());
+            System.out.println("CSS geladen: " + cssURL.toExternalForm());
+        } else {
+            System.out.println("CSS-Datei nicht gefunden an: " + cssPath);
+        }
+
         primaryStage.setScene(scene);
         primaryStage.sizeToScene();
-        primaryStage.setWidth(800); // Width of the window
-        primaryStage.setHeight(600); // Height of the window
+        primaryStage.setWidth(800); // Breite des Fensters
+        primaryStage.setHeight(600); // Höhe des Fensters
 
         primaryStage.setMinWidth(primaryStage.getWidth());
         primaryStage.setMinHeight(primaryStage.getHeight());
-        // Set the title of the stage
-        primaryStage.setTitle("Routplanner");
-        // Show the stage
+        primaryStage.setTitle("Routplanner"); // Titel des Fensters
         primaryStage.show();
         return root;
     }
+
 
     public static void showMapInDefaultBrowser(){
         hostServices.showDocument(Main.class.getResource("/at/fhtw/routplanner/html/leaflet.html").toExternalForm());
